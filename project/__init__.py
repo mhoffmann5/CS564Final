8bca0db784657eff1e407546d6ea3fd8027e0cb5
Michael Hoffmann <mhoffmann5@wisc.edu>
2020-07-21 10:48:55 -0500

    User registration works

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
@@ -12,6 +12,14 @@ def create_app():
 
     db.init_app(app)
 
+    login_manager=LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
